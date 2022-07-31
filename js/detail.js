d44fafb5dfff82eb4249eed0360399b78d40d2e3
ducthecyber <phuongduducthe@gmail.com>
2022-07-31 22:19:16 +0700

    Buidl giao dien 100% - Load Api

--- js/detail.js
+++ js/detail.js
@@ -1,24 +1,76 @@
-function layDanhSachProductApi() {
+var arrSanPham = [];
+function layThongTinSanPhamApi() {
     var promise = axios({
         url: 'https://shop.cyberlearn.vn/api/Product/getbyid?id=5',
         method: 'GET'
     });
     //Xử lý thành công
     promise.then(function (result) {
-        console.log(result.data);
-        //lấy dữ liệu dạng mảng từ server gán cho mảng product vừa tạo
-        arrProduct = result.data;
-        //kết quả trả ra object có vị trí "1" trong chuỗi Product
-        //product: id, name, price,img, description
-        console.log(arrProduct);
-        
+        console.log(result.data.content);
+        var arrSanPham = result.data.content;
+        var html = '';
+        document.querySelector('#name').innerHTML = arrSanPham.name;
+        document.querySelector('#description').innerHTML = arrSanPham.description;
+        document.querySelector('#price').innerHTML = arrSanPham.price;
+        document.querySelector('#quantity').value = Number(arrSanPham.quantity);
+        document.querySelector('#image').src = arrSanPham.image;
+        var sizeArray = arrSanPham.size;
+        //tạo nút Size
+        for (index = 0; index < sizeArray.length; index++) {
+            html += `
+            <button>
+                <span>${sizeArray[index]}</span>
+            </button>
+            `;
+        }
+        document.querySelector('#btnSize').innerHTML = html;
+        var relatedProduct = arrSanPham.relatedProducts;
+        var relatePro = '';
+        //tạo related product 
+        for (index = 0; index < relatedProduct.length; index++) {
+            relatePro += `
+            <div class="cell col-lg-4 col-md-6 col-sm-12 mt-5 mr-5">
+                <div class="card-top">
+                    <img id="image" src="${relatedProduct[index].image}" alt="" width="220" height="180" />
+                    <p id="name">${relatedProduct[index].name}</p>
+                    <p id="shortDescription">${relatedProduct[index].shortDescription}</p>
+                </div>
+                <div class="card-bottom" style="display:flex">
+                    <div class="cell-left">
+                        <a href="#">
+                            <button>
+                                <span>
+                                    Buy now
+                                </span>
+                            </button>
+                        </a>
+                    </div>
+                    <div class="cell-right">
+                        <span id="price">${relatedProduct[index].price}</span>
+                    </div>
+                </div>
+            </div>
+            `
+        }
+        document.querySelector('.row').innerHTML = relatePro;
     })
     //xử lý thất bại
     promise.catch(function (err) {
         console.log(err.data)
     });
 }
+//Tạo hàm gán thông tin sản phẩm vào giao diện
+function renderSanPham(idSanPham) {
+    var sanPham = new content();
+    console.log(sanPham);
+    document.querySelector('#name').innerHTML = sanPham.name;
+    document.querySelector('#description').innerHTML = sanPham.description;
+    document.querySelector('#price').innerHTML = sanPham.price;
+    document.querySelector('#quantity').innerHTML = sanPham.quantity;
+    document.querySelector('#size').innerHTML = sanPham.size;
+    document.querySelector('#image').innerHTML = sanPham.image;
+}
 //gọi hàm khi vừa load trang web
 window.onload = function () {
-    layDanhSachProductApi();
+    layThongTinSanPhamApi();
 }
---
+++ js/models/product.js
@@ -0,0 +1,12 @@
+//tao ra kieu du lieu san pham
+function content() {
+    this.id = '';
+    this.name = '';
+    this.price = '';
+    this.description = '';
+    this.size = '';
+    this.shortDescription = '';
+    this.quantity = '';
+    this.image = '';
+    this.relatedProducts='';
+}
